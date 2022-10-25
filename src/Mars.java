76fa5461a760a9b52c5344fbc732d7b734694370
ckyle121 <carolinekyle21@gmail.com>
2022-10-25 15:40:29 -0500

    inital file set up and Mars java file

---
+++ src/Mars.java
@@ -0,0 +1,12 @@
+public class Mars {
+    public static void main(String[] args) {
+    
+        String colonyName = "Gazorpazorp";
+
+        int shipPopulation = 300; 
+
+        double shipFood = 4000.00;
+
+        boolean Landing = true;
+    }
+}
